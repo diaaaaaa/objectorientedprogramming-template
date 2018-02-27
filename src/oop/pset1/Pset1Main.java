2b267f148766610974d80dd7ffccc32a2702689c
diaaaaaa <31830007+diaaaaaa@users.noreply.github.com>
2018-02-27 03:58:12 +0100

    version 1.0

--- src/oop/pset1/Pset1Main.java
+++ src/oop/pset1/Pset1Main.java
@@ -1,22 +1,36 @@
 package oop.pset1;
 
 import oop.pset1.controller.MovieDatabaseSummarizer;
-import oop.pset1.view.Summary;
+import oop.pset1.model.Actor;
+import oop.pset1.model.Movie;
+import oop.pset1.model.Summary;
+import oop.pset1.parser.ActorParser;
+import oop.pset1.parser.MovieParser;
 import oop.pset1.view.SummaryDisplayer;
 
-import java.util.Optional;
+import java.util.List;
+
 
 public class Pset1Main {
 
     public static void main(String[] args) {
-        MovieDatabaseSummarizer summarizer = new MovieDatabaseSummarizer();
-        SummaryDisplayer displayer = new SummaryDisplayer();
+        MovieParser parserMovie = new MovieParser();
+        ActorParser parserActor = new ActorParser();
 
-        Optional<Summary> summary = summarizer.summarize();
-        if(summary.isPresent()){
-            displayer.display(summary.get());
-        } else {
-            displayer.display();
-        }
+        List<Movie> movies = parserMovie.getMovies();
+        List<Actor> actors = parserActor.getActors();
+
+        MovieDatabaseSummarizer summarizerMovie = new MovieDatabaseSummarizer();
+        MovieDatabaseSummarizer summarizerActor = new MovieDatabaseSummarizer();
+
+        SummaryDisplayer displayerMovie = new SummaryDisplayer();
+        SummaryDisplayer displayerActor = new SummaryDisplayer();
+
+        Summary summaryMovie = summarizerMovie.summarizeMovie(movies);
+        Summary summaryActor = summarizerActor.summarizeActor(actors);
+        displayerMovie.displayMovie(summaryMovie);
+        displayerActor.displayActor(summaryActor);
+
+
     }
 }
---
+++ src/oop/pset1/parser/ActorParser.java
@@ -0,0 +1,71 @@
+package oop.pset1.parser;
+import oop.pset1.model.Actor;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ActorParser {
+    public List<Actor> getActors() {
+        String filePath = "src/oop/pset1/data/actors.csv";
+        try {
+            Stream<String> lines = Files.lines(Paths.get(filePath));
+            return lines.skip(1)
+                    .map(line -> line.split(";"))
+                    .filter(column -> column.length == 3)
+                    .filter(field -> field != null)
+                    .map(toActor())
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+    }
+
+    private Function<String[], Actor> toActor() {
+        return columns -> {
+            Actor actor = new Actor();
+            actor.setMostHiredActors(toRealActor(columns[0]));
+            actor.setActorsMaleFemaleRatio(toGender(columns[0]));
+            return actor;
+        };
+    }
+
+    private List<String> toRealActor(String column) {
+        String objects = column.replaceAll("\\[", "").replaceAll("]", "");
+        objects = objects.replaceAll("\\{", "").replaceAll("}", "");
+        objects = objects.replaceAll("'", "");
+
+        String[] words = objects.split(", ");
+        return Stream.of(words)
+                .filter(word -> !word.contains("character:"))
+                .map(word -> word.split(": "))
+                .filter(word -> word != null)
+                .filter(line -> line.length == 2 )
+                .map(word -> word[1])
+                .collect(Collectors.toList());
+
+    }
+    private List<String> toGender(String column) {
+        String objects = column.replaceAll("\\[", "").replaceAll("]", "");
+        objects = objects.replaceAll("\\{", "").replaceAll("}", "");
+        objects = objects.replaceAll("'", "");
+
+        String[] words = objects.split(", ");
+        return Stream.of(words)
+                .filter(word -> word.contains("gender:"))
+                .map(word -> word.split(": "))
+                .filter(word -> !word[1].contains("0"))
+                //.peek(word -> System.out.println(word[1]))
+                .filter(word -> word != null)
+                .filter(line -> line.length == 2 )
+                .map(word -> word[1])
+                //.peek(s -> System.out.println(s +" hooon"))
+                .collect(Collectors.toList());
+
+    }
+}
