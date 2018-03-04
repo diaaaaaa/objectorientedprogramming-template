58298981c8a4dc8fd8d931406eaafd364ad6978d
diaaaaaa <31830007+diaaaaaa@users.noreply.github.com>
2018-03-04 11:46:59 +0100

    BurgerHouse greeting&sayGoodbye

--- src/oop/pset2/burgerhouse/view/ShowMenu.java
+++ src/oop/pset2/burgerhouse/view/ShowMenu.java
@@ -1,15 +1,26 @@
 package oop.pset2.burgerhouse.view;
 
+import oop.pset2.burgerhouse.controller.BurgerHouseRestaurant;
+
 public class ShowMenu {
 
     public void showMenu(){
-        System.out.println("===================* Restaurant *====================\n");
-        System.out.println("Welcome to our Restaurant");
+        System.out.println("===================* Restaurant *====================");
+        greeting();
 
+        sayGoodbye();
+        System.out.println("===================* Restaurant *====================");
+
+
+
+    }
+
+    private void sayGoodbye() {
         System.out.println(" Goodbye ");
-        System.out.println("===================* Restaurant *====================\n");
 
+    }
 
-
+    private void greeting() {
+        System.out.println("Welcome to our Restaurant");
     }
 }
